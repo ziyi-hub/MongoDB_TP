eb0c68bd16c07b743015b88044e11d98140d0a85
Tania Olivia <taniaolivia9@gmail.com>
2021-12-21 17:34:05 +0100

    modification du fichier index.php

--- public/index.php
+++ public/index.php
@@ -5,9 +5,8 @@ require_once '../src/vendor/autoload.php';
 $c = new MongoDB\Client('mongodb://mongo:mongopass@mongo');
 $db = $c->mongodb;
 
-$url = file_get_contents('https://geoservices.grand-nancy.org/arcgis/rest/services/public/VOIRIE_Parking/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=nom%2Cadresse%2Cplaces%2Ccapacite&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson');
-$data = json_decode($url);
-//$db->createCollection('parkingCollection');
+$jsonData = file_get_contents('parkingCollection.json');
+$data = json_decode($jsonData);
 
 $collections = $db->listCollections();
 $collectionNames = [];
@@ -25,16 +24,17 @@ if (!($exists))
 
 $parkings = $db->parkingCollection->find();
 
+
 if(is_null($parkings)){
     print "no data";
     die();
 }
 
 foreach ($parkings as $parking) {
-    $feature = $parking->features;
+    $datas[] = $parking;
 }
 
-print_r(json_encode($feature));
+print_r(json_encode($datas));
 
 include 'index.html';
 ?>
